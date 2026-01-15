efdf21aa63cb41d27595b862729ccdb0d02a773c
RiccardoAccatino <20054233@studenti.uniupo.it>
2026-01-15 11:09:48 +0100

    Aggiunta di un articolo alla lista

--- ListaDiArticoli/src/gui/riga_comando/RigaComandoInterfaccia.java
+++ ListaDiArticoli/src/gui/riga_comando/RigaComandoInterfaccia.java
@@ -1,13 +1,12 @@
 package gui.riga_comando;
 import jbook.util.Input;
 import modello.GestioneListe;
+import modello.exception.ArticoloException;
+import modello.exception.GestioneListeException;
 public class RigaComandoInterfaccia {
-
-	private GestioneListe gestor;
 	private Input input;
 	public RigaComandoInterfaccia()
 	{
-		gestor = new GestioneListe();
 		input = new Input();
 		Menu();
 		
@@ -29,7 +28,11 @@ public class RigaComandoInterfaccia {
 		else if(sc==2)
 			MenuLista();
 		else if(sc==3)
-			AggiuntaProdotto();
+			try {
+				AggiuntaProdotto();
+			} catch (ArticoloException e) {
+				
+			}
 		else if(sc==4)
 			AggiuntaCategoria();
 		else if(sc==5)
@@ -39,25 +42,99 @@ public class RigaComandoInterfaccia {
 	}
 
 	private void AggiuntaCategoria() {
-		// TODO Auto-generated method stub
+		String nome="";
+		nome= input.readString("|Inserisci il nome della categoriaa che desideri creare=");
+		try{
+			GestioneListe.createCategoria(nome);
+		}catch(GestioneListeException e)
+		{
+			System.out.println("Errore durante la crezione della categoria!");
+		}
+		
+		Menu();
 		
 	}
 
-	private void AggiuntaProdotto() {
-		// TODO Auto-generated method stub
+	private void AggiuntaProdotto() throws ArticoloException {
+		String nome="";
+		nome= input.readString("|Inserisci il nome del prodotto che desideri creare=");
+		try{
+			GestioneListe.createArticolo(nome);
+		}catch(GestioneListeException e)
+		{
+			System.out.println("Errore durante la crezione dell'articolo!");
+		}
+		
+		Menu();
 		
 	}
 
 	private void MenuLista() {
+		int sc=0;
+		String nomeLis="";
+		nomeLis= input.readString("Inserisci il nome della lista che desideri selezionare=");
+		System.out.println("-----------"+nomeLis+"-------------");
+		System.out.println("|1-Aggiungi Prodotto");
+		System.out.println("|2-Cerca Prodotto");
+		System.out.println("|3-Sposta Prodotto nel cestino");
+		System.out.println("|4-Ripristina Prodotto");
+		System.out.println("|5-Svuota Cestino");
+		System.out.println("|6-Torna Indietro");
+		System.out.println("-----------------------------------------");
+		sc=input.readInt("|Inserisci=");
+		System.out.println("-----------------------------------------");
+		if(sc==1)
+			AggiungiProdottoAllaLista();
+		else if(sc==2)
+			CercaProdottoNellaLista();
+		else if(sc==3)
+			SpostaProdottoDallaLista();
+		else if(sc==4)
+			RipristinaProdotto();
+		else if(sc==5)
+			SvuotaCesto();
+		else if(sc==6)
+			Menu();
+		else
+			MenuLista();
+	}
+
+	private void SvuotaCesto() {
 		// TODO Auto-generated method stub
+		
+	}
+
+	private void RipristinaProdotto() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void SpostaProdottoDallaLista() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void CercaProdottoNellaLista() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void AggiungiProdottoAllaLista() {
+		String prd="";
+		prd=input.readString("Inserisci il nome del prodotto che vuoi aggiungere lla lista=");
 		
 	}
 
 	private void CrezioneLista() {
 		
 		String nome="";
-		nome= input.readString("Inserisci il nome dell lista che desideri creare=");
-		gestor.createListaDiArticoli(nome);
+		nome= input.readString("|Inserisci il nome dell lista che desideri creare=");
+		try{
+			GestioneListe.createListaDiArticoli(nome);
+		}catch(GestioneListeException e)
+		{
+			System.out.println("Errore durante la crezione della lista!");
+		}
 		
 		Menu();
 	}
